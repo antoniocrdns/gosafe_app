28b4b0e494c6c92cd532e5251a015a6a1835a82e
Antonio Cardenas <gohan21.d@gmail.com>
2025-03-11 12:59:16 -0700

    update api url

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api-gosafe.onrender.com/api',
+  baseURL: 'http://kscggogk8sw0kkokwg04gco0.31.170.165.191.sslip.io/api',
 });
 
 export default api;
